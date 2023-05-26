1214a59a25d6adbb2bd2b20a1080b7b3ffa2c133
Lars Ellingsen <lellingsen17@gmail.com>
2023-05-26 15:49:11 -0700

    updating color scheme of code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,10 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://lars-ellingsen.dev',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			theme: 'nord',
+			wrap: true,
+		},
+	},
 });
