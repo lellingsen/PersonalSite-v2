8d28c5a908527a52b490cc2fe1044453d345b416
Lars Ellingsen <lellingsen17@gmail.com>
2023-05-27 08:47:54 -0700

    updating title/description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Lars Ellingsen\'s Blog';
-export const SITE_DESCRIPTION = 'A collection of my thoughts and musings.';
+export const SITE_TITLE = "Lars's musings";
+export const SITE_DESCRIPTION = 'A collection of my thoughts and observations.';
